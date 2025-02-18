3b26c5ade02ad380bcd004dc527b26389965fb2e
Ian Zdanowsky <ianzpimentel@gmail.com>
2025-02-18 13:34:24 -0300

    chore: Add WHISPER_URL to environment configuration

--- src/lib/whisper/whisper.ts
+++ src/lib/whisper/whisper.ts
@@ -2,7 +2,9 @@ export function fetchFromWhisper(blob: Blob): Promise<Response> {
     const form = new FormData();
     form.append('timestamp', new Date().toISOString());
     form.append('audio', blob);
-    const result = fetch('http://127.0.0.1:9090/audioToText', {
+    const whisperUrl = process.env.WHISPER_URL
+    const whisperUrlAudioToText = whisperUrl + '/audioToText'
+    const result = fetch(whisperUrlAudioToText, {
         method: 'POST',
         body: form
     });
