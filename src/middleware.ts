1d7ebac8f28dff9e6aabe4f273839496bb96e6c5
Ian Zdanowsky <ianzpimentel@gmail.com>
2025-02-28 14:53:26 -0300

    refactor: Update middleware to redirect to /pesquisa for logged-in users on the homepage

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,18 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   // Get token from cookies (session) or Authorization header
+  const basePath = req.nextUrl.origin;
+
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
 
   // If the user is NOT logged in and trying to access a protected page, redirect to /login
   if (!token && req.nextUrl.pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", req.url));
+    return NextResponse.redirect(new URL("/login", basePath));
   }
 
-  // if (req.nextUrl.pathname === "/" && token) {
-  //   return NextResponse.redirect(new URL("/pesquisa", req.url));
-  // }
+  if (req.nextUrl.pathname === "/" && token) {
+    return NextResponse.redirect(new URL("/pesquisa", basePath));
+  }
 
   return NextResponse.next();
 }
